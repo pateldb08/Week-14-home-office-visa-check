3ac069038ccb37dbe50a21ace0b6fa7daadfe084
pateldb08 <pateldb08@gmail.com>
2023-05-12 23:24:47 +0100

    Week-14-home-office-visa-check

---
+++ src/main/java/gov/uk/check/visa/pages/StartPage.java
@@ -0,0 +1,25 @@
+package gov.uk.check.visa.pages;
+
+import com.aventstack.extentreports.Status;
+import gov.uk.check.visa.customlisteners.CustomListeners;
+import gov.uk.check.visa.utility.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
+
+public class StartPage extends Utility {
+    public StartPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath =  "//a[normalize-space()='Start now']")
+    WebElement start;
+
+
+    public void clickStartNow(){
+        Reporter.log("Click on start now: " + start.toString());
+        clickOnElement(start);
+        CustomListeners.test.log(Status.PASS, "Click on start now");
+    }
+}
